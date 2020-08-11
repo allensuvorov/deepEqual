1e9013bc255fe3066dc539d56aaa3db1b7fc6b15
Allen <allenvs@mail.ru>
2020-08-12 00:16:36 +0300

    cleaned code a bit

--- app.js
+++ app.js
@@ -1,5 +1,40 @@
-// Best case: O(n)
-// Worst case: O(2^n)
+// Rules:
+// 1. Both obj1 and obj2 are of object type and are not null
+// 2. All keys and value of obj1 and obj2 are equal
+
+// Pseudo code:
+// 1. If 1 or 2 is either null or not an object, return 1 === 2
+// 2. else, if number of keys of 1 and 2 is not equal, return false
+// 3. else, compare corresponding keys and values of 1 and 2, return false if any comparision fails
+// 4. otherwise return true
+
+// Allen's solution
+function deepEqual(obj1, obj2) {
+    if (obj1 === null || obj2 === null || typeof(obj1) !== 'object' || typeof(obj2) !== 'object') { 
+        return obj1 === obj2;
+    } else { 
+        const keys1 = Object.keys(obj1), keys2 = Object.keys(obj2);
+        if (keys1.length === keys2.length) {
+            for (let i = 0; i < keys1.length; i++) {
+                if (keys1[i] !== keys2[i] || !deepEqual(obj1[keys1[i]], obj2[keys2[i]])) {
+                return false; 
+                };
+            };
+            return true;
+        };
+        return false;
+    };
+};
+
+
+let obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
+
 
 // Mansur's solution
 function deepEqual(lhs, rhs) {
@@ -16,41 +51,3 @@ function deepEqual(lhs, rhs) {
     }
     return lhs === rhs;
 }
-
-// Rules:
-// 1. Both obj1 and obj2 are of object type and are not null
-// 2. All keys and value of obj1 and obj2 are equal
-
-// Pseudo code:
-// 1. If 1 or 2 is either null or not an object, return 1 === 2
-// 2. else, if number of keys of 1 and 2 is not equal, return false
-// 3. else, compare corresponding keys and values of 1 and 2, return false if any comparision fails
-// 4. otherwise return true
-
-// Allen's solution
-
-function deepEqual(obj1, obj2) {
-  if (obj1 === null || obj2 === null || typeof(obj1) !== 'object' || typeof(obj2) !== 'object') { 
-    return obj1 === obj2;
-  } else { 
-    const keys1 = Object.keys(obj1), keys2 = Object.keys(obj2);
-    if (keys1.length === keys2.length) {
-      for (let i = 0; i < keys1.length; i++) {
-        if (keys1[i] !== keys2[i] || !deepEqual(obj1[keys1[i]], obj2[keys2[i]])) {
-          return false; 
-        }
-      }
-      return true;
-    }
-    return false;
-  }
-}
-
-
-let obj = {here: {is: "an"}, object: 2};
-console.log(deepEqual(obj, obj));
-// → true
-console.log(deepEqual(obj, {here: 1, object: 2}));
-// → false
-console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
-// → true
